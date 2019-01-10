7b2ae7fb179b6c430638020d5975870041359f02
JXY3511088jxy <765101769@qq.com>
2019-01-10 17:06:53 +0800

    20190110

---
+++ python基础知识/python基础/数据类型和变量.py
@@ -0,0 +1,35 @@
+'''
+1、整型；
+2、浮点型；
+3、字符串；
+4、布尔值；
+5、空值：none；
+6、变量：变量在程序中就是用一个变量名表示了，变量名必须是大小写英文、数字和_的组合，且不能用数字开头。
+
+Python3 中有六个标准的数据类型：
+
+Number（数字）
+String（字符串）
+List（列表）
+Tuple（元组）
+Set（集合）
+Dictionary（字典）
+Python3 的六个标准数据类型中：
+
+不可变数据（3 个）：Number（数字）、String（字符串）、Tuple（元组）；
+可变数据（3 个）：List（列表）、Dictionary（字典）、Set（集合）。
+'''
+# -*- coding: utf-8 -*-
+n = 123
+f = 456.789
+s1 = 'Hello World'
+s2 = 'Hello,\'Adam\''
+s3 = r'Hello,"Bart"'
+s4 = r'''Hello,
+Lisa!'''
+print (n)
+print (f)
+print (s1)
+print (s2)
+print (s3)
+print (s4)
